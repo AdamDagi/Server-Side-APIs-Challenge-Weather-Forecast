320a587d90ac5341ba65caada9ca664f368eb9ff
AdamDagi <mugutdinovshiha02@gmail.com>
2021-11-01 23:45:34 -0400

    whrote html code, created structure, styled all conteiners in css, conected js and added api key and whrote function to get data from openweathermap

---
+++ js/weather.js
@@ -0,0 +1,16 @@
+async function start() {
+    const url = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=ade0fb3053d09af11fa65cf8982f5830";
+
+    let response = await fetch(url);
+
+    if (response.ok) { // если HTTP-статус в диапазоне 200-299
+    // получаем тело ответа (см. про этот метод ниже)
+    const data = await response.json();
+    // console.log(json);
+    console.log(data.main)
+    } else {
+    alert("Ошибка HTTP: " + response.status);
+    };
+};
+
+start();
