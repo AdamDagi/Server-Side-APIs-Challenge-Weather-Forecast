a8d3e63eec9fb440669946ba1c390229c75c1a27
AdamDagi <mugutdinovshiha02@gmail.com>
2021-11-02 20:32:58 -0400

    added animation to backfront image, changed api key, whrote function search click button

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,9 @@
-async function start() {
-    const url = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=ade0fb3053d09af11fa65cf8982f5830";
+const inputCity = document.body.querySelector(".search_input");
+const searchButton = document.body.querySelector(".search_button");
+
+
+async function start(city) {
+    const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=ade0fb3053d09af11fa65cf8982f5830`;
 
     let response = await fetch(url);
 
@@ -13,4 +17,10 @@ async function start() {
     };
 };
 
-start();
+// start();
+
+searchButton.onclick = () => {
+const inputInfo = inputCity.value;
+start(inputInfo);
+inputCity.value = "";
+};
