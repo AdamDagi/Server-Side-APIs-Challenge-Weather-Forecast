e69811ae4b172bd17bbd5d35c9c253474c449354
AdamDagi <mugutdinovshiha02@gmail.com>
2021-11-06 19:08:29 -0400

    Added animation, icons coreccted size of boxes, added uv index function for color type

---
+++ README.md
@@ -0,0 +1,43 @@
+* Created HTML Code.
+
+* Created Repo and puled code.
+
+* Created style.css and index.js file.
+
+* Whrote main structure in html.
+
+* Styled the page with CSS.
+
+* Corrected the color design.
+
+* Connected JS to first API Key to take city name by lat and lon.
+
+* Whrote main JS constants and function.
+
+* Connected JS to second API Key to take city weather for current day and for next 5 days.
+
+* Whrote in JS cards and functions for future days forecast.
+
+* Fixed in JS City search history.
+
+* Added in CSS and after in JS weather Icons.
+
+* Added calendar data in JS using moment JS.
+
+* Added animation to all scructure of forecasts.
+
+* Whrote function for UV Index and UV index color.
+
+* Added Local Storage.
+
+GitHub Repo: https://github.com/AdamDagi/Weather_Forecast_Challenge
+URL Link: https://adamdagi.github.io/Weather_Forecast_Challenge/
+
+Web Page without City info Screen.
+![Screen1](https://user-images.githubusercontent.com/90221273/140626020-8b47684e-adff-4291-9fca-bc41489c374e.jpeg)
+
+Web Page with City search and info Screen.
+![Screen2](https://user-images.githubusercontent.com/90221273/140626037-d14fdbc3-4542-4984-a684-14cc344a8942.jpeg)
+
+Web Page smaler size Screen.
+![Screen3](https://user-images.githubusercontent.com/90221273/140626044-4a6a43f8-48f9-458c-afc9-0cb4e3f09a79.jpeg)
--- js/weather.js
+++ js/weather.js
@@ -8,6 +8,7 @@ const infoWind = document.querySelector(".info_wind");
 const infoHumidity = document.querySelector(".info_humidity");
 const infoUvIndex = document.querySelector(".info_uv_index");
 const weatherIcon = document.querySelector(".weather_icon");
+const historyName = [];
 let lat;
 let lon;
 // ----------------------------Card 1-----------------------------------
@@ -57,37 +58,8 @@ async function start(city) {
         lon = data.city.coord.lon;
         const city = data.city;
         const cityNameApi = city.name;
-        // const currentDayData = data.list[0].main;
-        // const humidity = currentDayData.humidity;
-        // const temp = Math.floor(currentDayData.temp - 273) + " " + "&#8451";
-        // const windObj = data.list[0].wind;
-        // const windSpeed = windObj.speed;
-        // const city = data.city;
-        // const cityNameApi = city.name;
-        // const dtTxt = data.list[0].dt_txt;
-        // // const time = moment().format("MM/DD/YYYY");
-        // const weather = data.list[0].weather[0];
 
         cityName.innerHTML = cityNameApi;
-        // calendarData.innerHTML = dtTxt;
-        // infoTemp.innerHTML = temp;
-        // weatherIcon.classList.add("weather" + weather.icon);
-        // infoWind.innerHTML = windSpeed;
-        // infoHumidity.innerHTML = humidity;
-        // // ----------------------------Card 1-----------------------------------
-        // const dayOneData = data.list[8].main;
-        // const humidityOne = dayOneData.humidity;
-        // const tempOne = Math.floor(dayOneData.temp - 273) + " " + "&#8451";
-        // const windObjOne = data.list[8].wind;
-        // const windSpeedOne = windObjOne.speed;
-        // const weatherOne = data.list[8].weather[0];
-        // const dtTxtOne = data.list[8].dt_txt;
-
-        // infoTempOne.innerHTML = tempOne;
-        // weatherIconOne.classList.add("weather" + weatherOne.icon);
-        // infoWindOne.innerHTML = windSpeedOne;
-        // infoHumidityOne.innerHTML = humidityOne;
-        // calendarDataOne.innerHTML = dtTxtOne;
     } else {
         alert("Ошибка HTTP: " + response.status);
     };
@@ -102,94 +74,154 @@ async function getUV() {
     if (response.ok) { 
         const data = await response.json();
         const currentDayData = data.current;
-        const humidity = currentDayData.humidity;
+        const humidity = (currentDayData.humidity) + " " + "💧";
         const temp = Math.floor(currentDayData.temp - 273) + " " + "&#8451";
-        const windSpeed = currentDayData.wind_speed;
+        const windSpeed = (currentDayData.wind_speed) + " " + "🌬";
         const time = moment().format("MM/DD/YYYY");
         const weather = currentDayData.weather[0];
         const index = (currentDayData.uvi) + " " + "%";
-
+        
         calendarData.innerHTML = time;
-        infoTemp.innerHTML = temp;
+        infoTemp.innerHTML = (temp) + "  " + "&#x1F321";
+        weatherIcon.removeAttribute('class');
         weatherIcon.classList.add("weather" + weather.icon);
         infoWind.innerHTML = windSpeed;
         infoHumidity.innerHTML = humidity;
         infoUvIndex.innerHTML = index;
+        if (currentDayData.uvi <= 2) {
+            infoUvIndex.innerHTML = index + " " + "🟢"
+        } else if (currentDayData.uvi > 2 && currentDayData.uvi <= 5) {
+            infoUvIndex.innerHTML = index + " " + "🟡"
+        } else if (currentDayData.uvi > 5 && currentDayData.uvi <= 7) {
+            infoUvIndex.innerHTML = index + " " + "🟠"
+        } else if (currentDayData.uvi > 7 && currentDayData.uvi <= 10) {
+            infoUvIndex.innerHTML = index + " " + "🔴"
+        };
         // ----------------------------Card 1-----------------------------------
         const dayOneData = data.daily[0];
-        const humidityOne = dayOneData.humidity;
+        const humidityOne = (dayOneData.humidity) + " " + "💧";
         const tempOne = Math.floor(dayOneData.temp.day - 273) + " " + "&#8451";
-        const windSpeedOne = dayOneData.wind_speed;
+        const windSpeedOne = (dayOneData.wind_speed) + " " + "🌬";
         const weatherOne = dayOneData.weather[0];
         const timeOne = moment().add(1, 'days').format("MM/DD/YYYY");
         const indexOne = (dayOneData.uvi) + " " + "%";
 
         calendarDataOne.innerHTML = timeOne;
-        infoTempOne.innerHTML = tempOne;
+        infoTempOne.innerHTML = (tempOne) + "  " + "&#x1F321";
+        weatherIconOne.removeAttribute('class');
         weatherIconOne.classList.add("weather" + weatherOne.icon);
         infoWindOne.innerHTML = windSpeedOne;
         infoHumidityOne.innerHTML = humidityOne;
         infoUvIndexOne.innerHTML = indexOne;
+        if (dayOneData.uvi <= 2) {
+            infoUvIndexOne.innerHTML = index + " " + "🟢"
+        } else if (dayOneData.uvi > 2 && dayOneData.uvi <= 5) {
+            infoUvIndexOnex.innerHTML = index + " " + "🟡"
+        } else if (dayOneData.uvi > 5 && dayOneData.uvi <= 7) {
+            infoUvIndexOne.innerHTML = index + " " + "🟠"
+        } else if (dayOneData.uvi > 7 && dayOneData.uvi <= 10) {
+            infoUvIndexOne.innerHTML = index + " " + "🔴"
+        };
         // ----------------------------Card 2-----------------------------------
         const dayTwoData = data.daily[1];
-        const humidityTwo = dayTwoData.humidity;
+        const humidityTwo = (dayTwoData.humidity) + " " + "💧";
         const tempTwo = Math.floor(dayTwoData.temp.day - 273) + " " + "&#8451";
-        const windSpeedTwo = dayTwoData.wind_speed;
+        const windSpeedTwo = (dayTwoData.wind_speed) + " " + "🌬";
         const weatherTwo = dayTwoData.weather[0];
         const timeTwo = moment().add(2, 'days').format("MM/DD/YYYY");
         const indexTwo = (dayTwoData.uvi) + " " + "%";
 
         calendarDataTwo.innerHTML = timeTwo;
-        infoTempTwo.innerHTML = tempTwo;
+        infoTempTwo.innerHTML = (tempTwo)  + "  " + "&#x1F321";
+        weatherIconTwo.removeAttribute('class');
         weatherIconTwo.classList.add("weather" + weatherTwo.icon);
         infoWindTwo.innerHTML = windSpeedTwo;
         infoHumidityTwo.innerHTML = humidityTwo;
         infoUvIndexTwo.innerHTML = indexTwo;
+        if (dayTwoData.uvi <= 2) {
+            infoUvIndexTwo.innerHTML = index + " " + "🟢"
+        } else if (dayTwoData.uvi > 2 && dayTwoData.uvi <= 5) {
+            infoUvIndexTwo.innerHTML = index + " " + "🟡"
+        } else if (dayTwoData.uvi > 5 && dayTwoData.uvi <= 7) {
+            infoUvIndexTwo.innerHTML = index + " " + "🟠"
+        } else if (dayTwoData.uvi > 7 && dayTwoData.uvi <= 10) {
+            infoUvIndexTwo.innerHTML = index + " " + "🔴"
+        };
         // ----------------------------Card 3-----------------------------------
         const dayThreeData = data.daily[2];
-        const humidityThree = dayThreeData.humidity;
+        const humidityThree = (dayThreeData.humidity) + " " + "💧";
         const tempThree = Math.floor(dayThreeData.temp.day - 273) + " " + "&#8451";
-        const windSpeedThree = dayThreeData.wind_speed;
+        const windSpeedThree = (dayThreeData.wind_speed) + " " + "🌬";
         const weatherThree = dayThreeData.weather[0];
         const timeThree = moment().add(3, 'days').format("MM/DD/YYYY");
         const indexThree = (dayThreeData.uvi) + " " + "%";
 
         calendarDataThree.innerHTML = timeThree;
-        infoTempThree.innerHTML = tempThree;
+        infoTempThree.innerHTML = (tempThree) + "  " + "&#x1F321";
+        weatherIconThree.removeAttribute('class');
         weatherIconThree.classList.add("weather" + weatherThree.icon);
         infoWindThree.innerHTML = windSpeedThree;
         infoHumidityThree.innerHTML = humidityThree;
         infoUvIndexThree.innerHTML = indexThree;
+        if (dayThreeData.uvi <= 2) {
+            infoUvIndexThree.innerHTML = index + " " + "🟢"
+        } else if (dayThreeData.uvi > 2 && dayThreeData.uvi <= 5) {
+            infoUvIndexThree.innerHTML = index + " " + "🟡"
+        } else if (dayThreeData.uvi > 5 && dayThreeData.uvi <= 7) {
+            infoUvIndexThree.innerHTML = index + " " + "🟠"
+        } else if (dayThreeData.uvi > 7 && dayThreeData.uvi <= 10) {
+            infoUvIndexThree.innerHTML = index + " " + "🔴"
+        };
         // ----------------------------Card 4-----------------------------------
         const dayFourData = data.daily[3];
-        const humidityFour = dayFourData.humidity;
+        const humidityFour = (dayFourData.humidity) + " " + "💧";
         const tempFour = Math.floor(dayFourData.temp.day - 273) + " " + "&#8451";
-        const windSpeedFour = dayFourData.wind_speed;
+        const windSpeedFour = (dayFourData.wind_speed) + " " + "🌬";
         const weatherFour = dayFourData.weather[0];
         const timeFour = moment().add(4, 'days').format("MM/DD/YYYY");
         const indexFour = (dayFourData.uvi) + " " + "%";
 
         calendarDataFour.innerHTML = timeFour;
-        infoTempFour.innerHTML = tempFour;
+        infoTempFour.innerHTML = (tempFour) + "  " + "&#x1F321";
+        weatherIconFour.removeAttribute('class');
         weatherIconFour.classList.add("weather" + weatherFour.icon);
         infoWindFour.innerHTML = windSpeedFour;
         infoHumidityFour.innerHTML = humidityFour;
         infoUvIndexFour.innerHTML = indexFour;
+        if (dayFourData.uvi <= 2) {
+            infoUvIndexFour.innerHTML = index + " " + "🟢"
+        } else if (dayFourData.uvi > 2 && dayFourData.uvi <= 5) {
+            infoUvIndexFour.innerHTML = index + " " + "🟡"
+        } else if (dayFourData.uvi > 5 && dayFourData.uvi <= 7) {
+            infoUvIndexFour.innerHTML = index + " " + "🟠"
+        } else if (dayFourData.uvi > 7 && dayFourData.uvi <= 10) {
+            infoUvIndexFour.innerHTML = index + " " + "🔴"
+        };
         // ----------------------------Card 5-----------------------------------
         const dayFiveData = data.daily[4];
-        const humidityFive = dayFiveData.humidity;
+        const humidityFive = (dayFiveData.humidity) + " " + "💧";
         const tempFive = Math.floor(dayFiveData.temp.day - 273) + " " + "&#8451";
-        const windSpeedFive = dayFiveData.wind_speed;
+        const windSpeedFive = (dayFiveData.wind_speed) + " " + "🌬";
         const weatherFive = dayFiveData.weather[0];
         const timeFive = moment().add(5, 'days').format("MM/DD/YYYY");
         const indexFive = (dayFiveData.uvi) + " " + "%";
 
         calendarDataFive.innerHTML = timeFive;
-        infoTempFive.innerHTML = tempFive;
+        infoTempFive.innerHTML = (tempFive) + "  " + "&#x1F321";
+        weatherIconFive.removeAttribute('class');
         weatherIconFive.classList.add("weather" + weatherFive.icon);
         infoWindFive.innerHTML = windSpeedFive;
         infoHumidityFive.innerHTML = humidityFive;
         infoUvIndexFive.innerHTML = indexFive;
+        if (dayFiveData.uvi <= 2) {
+            infoUvIndexFive.innerHTML = index + " " + "🟢"
+        } else if (dayFiveData.uvi > 2 && dayFiveData.uvi <= 5) {
+            infoUvIndexFive.innerHTML = index + " " + "🟡"
+        } else if (dayFiveData.uvi > 5 && dayFiveData.uvi <= 7) {
+            infoUvIndexFive.innerHTML = index + " " + "🟠"
+        } else if (dayFiveData.uvi > 7 && dayFiveData.uvi <= 10) {
+            infoUvIndexFive.innerHTML = index + " " + "🔴"
+        };
     } else {
         alert("Ошибка HTTP: " + response.status);
     };
@@ -200,12 +232,19 @@ searchButton.onclick = async () => {
     if (!inputInfo.length) {
         return;
     };
+    if (historyName.indexOf(inputInfo) != -1) {
+        await start(inputInfo);
+        await getUV();
+        inputCity.value = "";
+        return;
+    };
     const elementDiv = document.createElement("div");
     elementDiv.className = "citi_history_line";
     const text = document.createElement("p");
     text.className = "city_history_name";
 
     text.innerHTML = inputInfo;
+    historyName.push(inputInfo);
     elementDiv.append(text);
     elementDiv.onclick = async (e) => historySearch(e);
     searchCityHistory.insertAdjacentElement("afterbegin", elementDiv);
@@ -218,3 +257,5 @@ async function historySearch (e) {
     await start(e.target.innerHTML);
     await getUV();
 };
+
+
